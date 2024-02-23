7a06ea74d184bd272f460f6bb0a8784fe8a02cda
dqdNotional <datquocha@notional.ventures>
2024-02-23 08:24:09 +0700

    Update gitops on docker swarm with portainer ( ansible.sh, install portainer playbook )

--- gitops-on-docker-swarm-with-portainer/ansible.sh
+++ gitops-on-docker-swarm-with-portainer/ansible.sh
@@ -6,3 +6,4 @@ ansible-playbook ./playbook_add_manager.yaml -i inventory
 ansible-playbook ./playbook_add_worker.yaml -i inventory
 ansible-playbook ./playbook_get_nodes.yaml -i inventory
 ansible-playbook ./playbook_install_portainer.yaml -i inventory
+ansible-playbook ./playbook_find_location.yaml -i inventory
