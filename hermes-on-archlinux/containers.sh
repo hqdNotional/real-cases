fb4cfad106483edfbc79456c2de7cbfd321513c9
dqdNotional <datquocha@notional.ventures>
2024-03-05 12:44:39 +0700

    Update containers.sh ( add commented commands that run built images )

--- hermes-on-archlinux/containers.sh
+++ hermes-on-archlinux/containers.sh
@@ -3,3 +3,7 @@
 docker run -it -d --hostname cosmos1 --name cosmos1 --net host archlinux:latest
 docker run -it -d --hostname cosmos2 --name cosmos2 --net host archlinux:latest
 docker run -it -d --hostname hermes --name hermes --net host archlinux:latest
+
+# docker run -it -d --hostname cosmos1 --name cosmos1 --net host hermes:latest
+# docker run -it -d --hostname cosmos2 --name cosmos2 --net host cosmos:latest
+# docker run -it -d --hostname hermes --name hermes --net host cosmos:latest
